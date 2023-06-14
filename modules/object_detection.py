d3732f63ad65cd84d6b3b721d03b8a456a92087d
hajin-park <hajin1819@gmail.com>
2023-06-14 04:39:21 -0700

    Code refactoring, documentation, edits (potential breaks)

--- modules/object_detection.py
+++ modules/object_detection.py
@@ -5,7 +5,7 @@ import cv2
 
 @dataclass(order=True)
 class Stair:
-    '''stair objects found on the screenshot'''
+    """stair objects found on the screenshot"""
 
     w: int = field(repr=False, init=False, default=87)
     h: int = field(repr=False, init=False, default=47)
@@ -15,7 +15,7 @@ class Stair:
 
 @dataclass
 class SearchArea:
-    '''segments of the screenshot searched for stair images'''
+    """segments of the screenshot searched for stair images"""
 
     dimensions: list
     x: int
@@ -23,110 +23,101 @@ class SearchArea:
 
 
 class ObjectDetection:
-    '''search the screenshot for stair images and find valid stair paths'''
+    """search the screenshot for stair images and find valid stair paths"""
 
     STAIR_THRESHOLD = 0.99
     screenshot = None
     stairs_detected = list()
 
-    def __init__(self,
-                 stair_images: list,
-                 horizontal_layers: list,
-                 vertical_layers: list) -> None:
-        '''Initialize needle images and screenshot layers.'''
+    def __init__(
+        self, stair_images: list, horizontal_layers: list, vertical_layers: list
+    ) -> None:
+        """Initialize needle images and screenshot layers."""
 
         self.horizontal_layers = horizontal_layers
         self.vertical_layers = vertical_layers
         self.stair_images = stair_images
 
-    def update(self, screenshot: list, ) -> None:
-        '''Update the current screenshot, bot state, and player's direction.'''
+    def update(
+        self,
+        screenshot: list,
+    ) -> None:
+        """Update the current screenshot, bot state, and player's direction."""
 
         self.screenshot = screenshot
 
-    def find_needles(self,
-                     haystack: list,
-                     needles: list,
-                     dimensions: list) -> SearchArea:
-        '''Find an object with a value above a threshold in a screenshot segment.'''
-
-        for needle in needles:
-            match = cv2.matchTemplate(haystack, needle, cv2.TM_CCORR_NORMED)
+    def search_area(self, dimensions: list, layer: int) -> bool:
+        """Search a specified region for a stair"""
+        area_sct = self.screenshot[
+            dimensions[0] : dimensions[1], dimensions[2] : dimensions[3]
+        ]
+        for needle in self.stair_images:
+            match = cv2.matchTemplate(area_sct, needle, cv2.TM_CCORR_NORMED)
             result = cv2.minMaxLoc(match)
             if result[1] > self.STAIR_THRESHOLD:
-                return SearchArea(dimensions, result[3][0], result[1])
-
-    def search_area(self,
-                    dimensions: list,
-                    layer: int,
-                    whole: bool = False) -> bool:
-        '''Search a specified region for a stair'''
-
-        #   search a screenshot segment
-        area_sct = self.screenshot[dimensions[0]                                   : dimensions[1], dimensions[2]: dimensions[3]]
-        area = self.find_needles(area_sct, self.stair_images, dimensions)
-        if area is not None:
-
-            #   use the vertical layer of the screenshot segment
-            if not whole:
-                x_loc = self.vertical_layers.index(dimensions[3])
+                x_loc = (
+                    np.searchsorted(self.vertical_layers, result[3][0], side="right")
+                    if dimensions[2] == self.vertical_layers[0]
+                    else self.vertical_layers.index(dimensions[3])
+                )
                 self.stairs_detected.append(Stair(x_loc, layer))
                 return True
-
-            #   find the vertical layer of the stair if the whole layer is searched
-            x_loc = np.searchsorted(self.vertical_layers, area.x, side="right")
-            self.stairs_detected.append(Stair(x_loc, layer))
-            return True
-
         return False
 
-    def search_segmented_layer(self,
-                               top: int,
-                               bottom: int,
-                               x_pos: int,
-                               layer: int) -> None:
-        '''Search a layer of a screenshot in segments for stair objects.'''
-
-        #   Search left screenshot segment
-        left_area_dimensions = [
-            top, bottom, self.vertical_layers[x_pos-2], self.vertical_layers[x_pos-1]]
-        if self.search_area(left_area_dimensions, layer):
-            return
-
-        #   Search right screenshot segment if necessary
-        right_area_dimensions = [
-            top, bottom, self.vertical_layers[x_pos], self.vertical_layers[x_pos+1]]
-        if self.search_area(right_area_dimensions, layer):
-            return
-
     def main(self) -> None:
-        '''search each layer algorithmically for stair objects.'''
-
+        """search each layer algorithmically for stair objects."""
         self.stairs_detected.clear()
-
-        #   search both sides of the player on the first layer
-        self.search_segmented_layer(
-            self.horizontal_layers[11], self.horizontal_layers[12], 4, 12)
+        self.search_area(
+            [
+                self.horizontal_layers[11],
+                self.horizontal_layers[12],
+                self.vertical_layers[2],
+                self.vertical_layers[5],
+            ],
+            12,
+        )
         if not self.stairs_detected:
             return
-        for i, layer in reversed(list(enumerate(self.horizontal_layers[1:-1], start=1))):
-
-            #   search the whole layer if stairs skip a layer
-            if self.stairs_detected[-1].y-i != 1:
-                self.search_area([self.horizontal_layers[i-1], layer,
-                                 self.vertical_layers[0], self.vertical_layers[7]], i, True)
-
-            #   search both sides of a stair not touching the screen edge
+        for i, layer in reversed(
+            list(enumerate(self.horizontal_layers[1:-1], start=1))
+        ):
+            if self.stairs_detected[-1].y - i != 1:
+                self.search_area(
+                    [
+                        self.horizontal_layers[i - 1],
+                        layer,
+                        self.vertical_layers[0],
+                        self.vertical_layers[7],
+                    ],
+                    i,
+                )
             elif self.stairs_detected[-1].x not in (1, 7):
-                self.search_segmented_layer(
-                    self.horizontal_layers[i-1], layer, self.stairs_detected[-1].x, i)
-
-            #   search the right side of a stair touching the left screen edge
+                self.search_area(
+                    [
+                        self.horizontal_layers[i - 1],
+                        layer,
+                        self.vertical_layers[self.stairs_detected[-1].x - 2],
+                        self.vertical_layers[self.stairs_detected[-1].x + 1],
+                    ],
+                    i,
+                )
             elif self.stairs_detected[-1].x == 1:
-                self.search_area([self.horizontal_layers[i-1], layer,
-                                 self.vertical_layers[1], self.vertical_layers[2]], i)
-
-            #   search the left side of a stair touching the right screen edge
+                self.search_area(
+                    [
+                        self.horizontal_layers[i - 1],
+                        layer,
+                        self.vertical_layers[0],
+                        self.vertical_layers[2],
+                    ],
+                    i,
+                )
             elif self.stairs_detected[-1].x == 7:
-                self.search_area([self.horizontal_layers[i-1], layer,
-                                 self.vertical_layers[5], self.vertical_layers[6]], i)
+                self.search_area(
+                    [
+                        self.horizontal_layers[i - 1],
+                        layer,
+                        self.vertical_layers[5],
+                        self.vertical_layers[7],
+                    ],
+                    i,
+                )
--- modules/pathfinder.py
+++ modules/pathfinder.py
@@ -1,149 +1,74 @@
+from typing import List, Tuple
+
+
 class PathFinder:
-    '''Pathfind by filling in missing layers from a list of detected stairs.'''
+    """Pathfind by filling in missing layers from a list of detected stairs."""
 
-    screenshot = None
-    state = None
-    left = True
-    stairs_detected = list()
-    stairs_connected = list()
-    actions = list()
+    def __init__(self):
+        self.left = True
+        self.stairs_detected: List[Tuple[int, int]] = []
+        self.stairs_connected: List[Tuple[int, int]] = []
+        self.actions: List[str] = []
 
-    def update(self, left: bool, stairs_detected: list) -> None:
-        '''Update the current screenshot, bot state, and player's direction.'''
-
+    def update(self, left: bool, stairs_detected: List[Tuple[int, int]]) -> None:
+        """Update the current screenshot, bot state, and player's direction."""
         self.left = left
         self.stairs_detected = stairs_detected
 
-    def fill_edge(self, stair):
-        '''Fill in two layers off the screen'''
-
-        if (stair.x not in (1, 7)):
-            return True
-
-        #   stair is on the left screen edge
-        elif (stair.x == 1):
-            self.stairs_connected.append([stair.x-1, stair.y-1])
-            self.stairs_connected.append([stair.x-2, stair.y-2])
-
-        #   stair is on the right screen edge
-        elif (stair.x == 7):
-            self.stairs_connected.append([stair.x+1, stair.y-1])
-            self.stairs_connected.append([stair.x+2, stair.y-2])
-
-    def fill_last(self, stair, next_stair):
-        self.stairs_connected.append([next_stair.x, next_stair.y])
-        self.fill_edge(stair)
-
-    def fill_zero(self, delta_x, stair, next_stair, next_is_last):
-        if (delta_x not in (-1, 1)):
-            return True
-
-        if next_is_last:
-            self.fill_last(stair, next_stair)
-
-    def fill_one(self, delta_x, stair, next_stair, next_is_last):
-        if (delta_x not in (-2, 2, 0)):
-            return True
-
-        #   current stair is next to next stair
-        elif (delta_x in (-2, 2)):
-            self.stairs_connected.append([stair.x-(delta_x/2), stair.y-1])
-
-        #   current stair is under next stair
-        elif (stair.x in (1, 5)):
-            self.stairs_connected.append([stair.x-1, stair.y-1])
-
-        #   current stair is under next stair
-        elif (stair.x in (3, 7)):
-            self.stairs_connected.append([stair.x+1, stair.y-1])
-
-        else:
-            return True
-
-        if next_is_last:
-            self.fill_last(stair, next_stair)
-
-    def fill_two(self, delta_x, stair, next_stair, next_is_last):
-        if (delta_x not in (-3, 3)):
-            return True
-
-        #   bottom stair is left of top stair
-        elif (delta_x == -3):
-            self.stairs_connected.append([stair.x+1, stair.y-1])
-            self.stairs_connected.append([stair.x+2, stair.y-2])
-
-        #   bottom stair is right of top stair
-        elif (delta_x == 3):
-            self.stairs_connected.append([stair.x-1, stair.y-1])
-            self.stairs_connected.append([stair.x-2, stair.y-2])
-
-        if next_is_last:
-            self.fill_last(stair, next_stair)
+    def fill_stairs(self, delta_x: int, delta_y: int, stair: Tuple[int, int]) -> None:
+        """Fill in the stairs based on the difference in x and y coordinates of the current and next stairs."""
+        if delta_y == 1 and delta_x in (-1, 1):
+            if delta_x == -1:
+                self.stairs_connected.append((stair[0] + 1, stair[1] - 1))
+            else:
+                self.stairs_connected.append((stair[0] - 1, stair[1] - 1))
+        elif delta_y == 2 and delta_x in (-2, 2, 0):
+            self.stairs_connected.append((stair[0] - delta_x // 2, stair[1] - 1))
+        elif delta_y == 3 and delta_x in (-3, 3):
+            self.stairs_connected.append((stair[0] + delta_x // 3, stair[1] - 1))
+            self.stairs_connected.append((stair[0] + 2 * delta_x // 3, stair[1] - 2))
 
     def fill_path(self) -> None:
-        '''Pathfind and fill in empty layers between detected stairs.'''
-
-        if (self.stairs_detected[0].y != 12 or self.stairs_detected[0].x not in (3, 5)):
+        """Pathfind and fill in empty layers between detected stairs."""
+        if self.stairs_detected[0][1] != 12 or self.stairs_detected[0][0] not in (3, 5):
             return
 
-        for (i, stair) in enumerate(self.stairs_detected[:-1], start=1):
-            next_stair = self.stairs_detected[i]
-            next_is_last = self.stairs_detected[-1] == next_stair
-            delta_x = stair.x - next_stair.x
-            delta_y = stair.y - next_stair.y
+        for i, stair in enumerate(self.stairs_detected[:-1]):
+            next_stair = self.stairs_detected[i + 1]
+            delta_x = stair[0] - next_stair[0]
+            delta_y = stair[1] - next_stair[1]
 
-            self.stairs_connected.append([stair.x, stair.y])
+            self.stairs_connected.append(stair)
 
-            #   end pathfinding if there an empty layer above a certain point
-            if (delta_y != 1 and stair.y < 7):
+            if delta_y != 1 and stair[1] < 7:
                 return
-
-            elif (delta_y == 1):
-                if self.fill_zero(delta_x, stair, next_stair, next_is_last):
-                    return
-
-            elif (delta_y == 2):
-                if self.fill_one(delta_x, stair, next_stair, next_is_last):
-                    return
-
-            elif (delta_y == 3):
-                if self.fill_two(delta_x, stair, next_stair, next_is_last):
-                    return
-
-            #   end pathfinding if there are more than two consecutive empty layers
             else:
-                return self.fill_edge(stair)
+                self.fill_stairs(delta_x, delta_y, stair)
 
     def next_action(self, current_stair: float, next_stair: int) -> str:
-        '''Queue an action for two consecutive stairs.'''
-
-        button = ''
-
-        if (next_stair < current_stair):
-            button = 'right' if self.left else 'left'
-        elif (next_stair > current_stair):
-            button = 'left' if self.left else 'right'
-        if (button == 'left'):
+        """Queue an action for two consecutive stairs."""
+        button = ""
+        if next_stair < current_stair:
+            button = "right" if self.left else "left"
+        elif next_stair > current_stair:
+            button = "left" if self.left else "right"
+        if button == "left":
             self.left = not self.left
 
         return button
 
     def queue_actions(self) -> None:
-        '''Queue actions based on the path created.'''
+        """Queue actions based on the path created."""
 
         if self.stairs_connected:
-
-            self.actions.append(self.next_action(
-                4, self.stairs_connected[0][0]))
-            for i in range(len(self.stairs_connected)-1):
-
+            self.actions.append(self.next_action(4, self.stairs_connected[0][0]))
+            for i in range(len(self.stairs_connected) - 1):
                 current_stair = self.stairs_connected[i][0]
-                next_stair = self.stairs_connected[i+1][0]
-                self.actions.append(self.next_action(
-                    current_stair, next_stair))
+                next_stair = self.stairs_connected[i + 1][0]
+                self.actions.append(self.next_action(current_stair, next_stair))
 
     def main(self) -> None:
-        '''main loop to reset and generate a new path'''
+        """main loop to reset and generate a new path"""
 
         self.stairs_connected.clear()
         self.actions.clear()
